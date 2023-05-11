4db822b20cd4c36d66cd86aa9d81e46b62db4379
wolfwork <wolfdate25@naver.com>
2023-05-11 23:34:14 +0900

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+  {
+    "Lazyvim/Lazyvim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.gotmpl = {
+        install_info = {
+          url = "https://github.com/msvechla/tree-sitter-go-template/tree/fix_brackets",
+          files = { "src/parser.c" },
+        },
+        filetype = "gotmpl",
+        used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+      }
+    end,
+  },
+}
