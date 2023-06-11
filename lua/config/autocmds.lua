d0487e3735e0b177173f57b70b3b2af23b3e893d
wolfdate25 <wolfdate25@naver.com>
2023-06-12 07:09:55 +0900

    add terraform treesitter
    Author: wolfdate25 <wolfdate25@naver.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+-- wrap and check for spell in text filetypes
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("commentstring"),
+  pattern = { "terraform" },
+  callback = function()
+    vim.opt.commentstring = "# %s"
+  end,
+})
